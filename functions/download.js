05140dc91bce391ebd793dc4004533c557d4c43d
steventian1101 <steventian1101@gmail.com>
2023-04-06 12:59:09 +0900

    Update download.js

--- functions/download.js
+++ functions/download.js
@@ -1,6 +1,6 @@
 const https = require("https");
 const axios = require("axios");
-const stripe = require('stripe')('sk_test_51IH8sJAZAQKiaOfYxdO4oKTRXeX0Nox65R7opwGOcSgxMDeQ42udiV9gvAGp8bH6MKW0mFUAVTlso0mIzZI17kEe00ifqlV1Mi');
+
 
 async function balance(){
    const balance = await stripe.balance.retrieve();
